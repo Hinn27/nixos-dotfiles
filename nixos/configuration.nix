4643040e08dcb7f75c49bc044fdf5747eb9d7a39
Hinn <duc107243@donga.edu.vn>
2026-08-05 21:20:23 +0700

    Sử dụng prime.sync thay vì offload để fix lỗi EGL Wayland

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -101,8 +101,7 @@
     open = false;
     nvidiaSettings = true;
     prime = {
-      offload.enable = true;
-      offload.enableOffloadCmd = true;
+      sync.enable = true;
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
     };
