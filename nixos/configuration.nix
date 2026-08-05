ae52b4ed97cc995a23d2aabd64c864300f42432d
Hinn <duc107243@donga.edu.vn>
2026-08-05 20:59:29 +0700

    Thêm lại rule NOPASSWD cho sudo

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -132,6 +132,19 @@
     };
   };
 
+  # Sudo
+  security.sudo.extraRules = [
+    {
+      users = [ "hinne" ];
+      commands = [
+        {
+          command = "ALL";
+          options = [ "NOPASSWD" ];
+        }
+      ];
+    }
+  ];
+
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
   services.openssh = {
