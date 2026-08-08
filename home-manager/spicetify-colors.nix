bd8146b3ad58216ecbda802a36315dca5d69fe54
Hinn <duc107243@donga.edu.vn>
2026-08-08 19:15:20 +0700

    Đổi màu Spotify

---
+++ home-manager/spicetify-colors.nix
@@ -0,0 +1,17 @@
+{
+  text = "cdd6f4";
+  subtext = "bac2de";
+  main = "1e1e2e";
+  sidebar = "181825";
+  player = "181825";
+  card = "1e1e2e";
+  shadow = "11111b";
+  selected-row = "313244";
+  button = "cba6f7";
+  button-active = "cba6f7";
+  button-disabled = "45475a";
+  tab-active = "313244";
+  notification = "f38ba8";
+  notification-error = "f38ba8";
+  misc = "9399b2";
+}
